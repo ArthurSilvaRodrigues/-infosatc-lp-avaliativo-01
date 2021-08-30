6b7f5a878fda2a9a7e394a756bee8c3f4fcd83e4
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-08-30 14:30:05 -0300

    Ativi-02-28

---
+++ Ativi-02-28.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+n3 = float(input('Digite outro valor: '))
+print('A soma dos quadrados dos três valores lidos é: {}'.format((n1**2)+(n2**2)+(n3**2)))
