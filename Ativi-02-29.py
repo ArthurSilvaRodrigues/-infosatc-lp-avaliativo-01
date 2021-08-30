892a86cf46c23eb733118d9af936ff02e0df9e18
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-08-30 14:36:05 -0300

    Ativi-02-29

---
+++ Ativi-02-29.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+n4 = float(input('Digite a quarta nota: '))
+print('A media das notas é: ' + 
+    '\nN1 = {}'.format(n1) +
+    '\nN2 = {}'.format(n2) +
+    '\nN3 = {}'.format(n3) +
+    '\nN4 = {}'.format(n4) +
+    '\nmedia = {}'.format((n1+n2+n3+n4)/4))
