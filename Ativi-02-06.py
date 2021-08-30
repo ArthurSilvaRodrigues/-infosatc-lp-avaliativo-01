4a2dee68452f0c0c158608586527188a4b9fb9f9
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-08-30 13:51:23 -0300

    Ativi-02-06
    atividade 6 da lista de 30 exercicios

---
+++ Ativi-02-06.py
@@ -0,0 +1,5 @@
+c = float(input('Digite a temperatura em °C para ser transformada: '))
+print('As temperaturas são igualadas: '        + 
+      '\nCelsios:    {} °C'.format(c)          +
+      '\nFahrenheit: {} °F'.format(c*(9/5)+32) +
+      '\nKelvvin:    {} K'.format(c+273.15))
