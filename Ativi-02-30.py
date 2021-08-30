1b629c8698ed1149cf34547190ded916191b8216
Arthur da Silva Rodrigues <silva.arthur2506@gmail.com>
2021-08-30 14:40:55 -0300

    Ativi-02-30

---
+++ Ativi-02-30.py
@@ -0,0 +1,3 @@
+r = float(input('Digite quantos reais você tem na carteira: '))
+d = float(input('Digite quantos esta a cotação do dolar: '))
+print('Com {:.2f} reais você tem {:.2f} dolares'.format(r,r/d))
